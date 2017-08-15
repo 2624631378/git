7ae2d9933ee92df3b78db82ba3f2cc54be7c20cb
Christian Couder <christian.couder@gmail.com>
2017-08-15 19:36:11 +0200

    sub-process: print the cmd when a capability is unsupported
    In handshake_capabilities() we use warning() when a capability
    is not supported, so the exit code of the function is 0 and no
    further error is shown. This is a problem because the warning
    message doesn't tell us which subprocess cmd failed.
    
    On the contrary if we cannot write a packet from this function,
    we use error() and then subprocess_start() outputs:
    
        initialization for subprocess '<cmd>' failed
    
    so we can know which subprocess cmd failed.
    
    Let's improve the warning() message, so that we can know which
    subprocess cmd failed.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sub-process.c
+++ sub-process.c
@@ -155,7 +155,8 @@ static int handshake_version(struct child_process *process,
 
 static int handshake_capabilities(struct child_process *process,
 				  struct subprocess_capability *capabilities,
-				  unsigned int *supported_capabilities)
+				  unsigned int *supported_capabilities,
+				  const char *cmd)
 {
 	int i;
 	char *line;
@@ -181,8 +182,8 @@ static int handshake_capabilities(struct child_process *process,
 			if (supported_capabilities)
 				*supported_capabilities |= capabilities[i].flag;
 		} else {
-			warning("external filter requested unsupported filter capability '%s'",
-				p);
+			warning("subprocess '%s' requested unsupported capability '%s'",
+				cmd, p);
 		}
 	}
 
@@ -203,8 +204,10 @@ int subprocess_handshake(struct subprocess_entry *entry,
 
 	retval = handshake_version(process, welcome_prefix, versions,
 				   chosen_version) ||
-		 handshake_capabilities(process, capabilities,
-					supported_capabilities);
+		 handshake_capabilities(process,
+					capabilities,
+					supported_capabilities,
+					entry->cmd);
 
 	sigchain_pop(SIGPIPE);
 	return retval;
